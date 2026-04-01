11d8c6815b80a803705ca8895a7ccb1b31219cd2
Luke Livingston <luke.livingston@colostate.edu>
2026-04-01 13:49:27 -0600

    Basic Format

---
+++ about.php
@@ -0,0 +1,36 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="parent">
+        <div class="div1"> 
+            <img src="Logo.jpg" style="width:100px">
+        </div>
+        <div class="div2">
+            <h1>Hello</h1>
+        </div>
+        <div class="div3">
+            <h1>Hello</h1>
+        </div>
+        <div class="div4">
+            <h1>Hello</h1>
+        </div>
+        <div class="div5">
+            <h1>Hello</h1>
+        </div>
+        <div class="div6">
+            <h1>Hello</h1>
+        </div>
+        <div class="div7">
+            <h1>hi</h1>
+        </div>
+    </div>
+</body>
+</html>
