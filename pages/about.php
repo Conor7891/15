831bd49e7c6c7621cc31947a244e06389d201c45
Aaron Barnes <adbarnes@hong-kong.cs.colostate.edu>
2026-04-07 14:13:38 -0600

    Conor, About Me

--- pages/about.php
+++ pages/about.php
@@ -38,10 +38,12 @@
             </p>
         </div>
         <div class="div7">
-            <img src="" alt=placeholder></img>
-            <h4>FIRST LAST</h4>
-            <h5>ROLE</h5>
-            <p>about me</p>
+            <img src="./images/ConorProfile.JPG" alt=profile photo of conor></img>
+            <h4>Conor Holland</h4>
+            <h5>Programmer</h5>
+            <p>Conor is a passionate developer who loves to create innovative solutions. Originally from Scotland, Conor brings a unique perspective to our development team.
+                Conor enjoys getting ripped, playing video games, and exploring new technologies in his free time.
+            </p>
         </div>
     </div>
 </body>
