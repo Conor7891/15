3d59302995206e2fe250533c635d917f524ad1c2
Luke Livingston <luke.livingston@colostate.edu>
2026-04-07 12:22:23 -0600

    Dynamic loading of pages

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+$page = $_POST['page'] ?? 'index';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,28 +10,37 @@
     <title>PixelPushers Color Generator</title>
     <link rel="stylesheet" href="./style/about.css">
 </head>
-<body>
-    <div class="parent">
+<header>
+    <nav>
         <div class="div1">
             <img src = "images/Logo.jpg" alt = "PixelPushers Logo" class = "logo">
             <h1>Welcome to the PixelPushers Color Generator!</h1>
         </div>
         <div class="div2">
             <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#Colour Coordinator">Colour Coordinator</a></li>
+                <form method="POST">
+                    <button name="page" value="index">Home</button>
+                    <button name="page" value="about">About Us</button>
+                    <button name="page" value="color">Color Coordinator</button>
+                </form>
             </ul>
         </div>
-        <div class="div3">
-            <h1>Meet the Team</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <button>Try the Color Coordinator</button>
-        </div>
-        <div class="div4"></div>
-        <div class="div5"></div>
-        <div class="div6"></div>
-    </div>
+    </nav>
+</header>
+<body>
+    <?php
+        switch ($page) {
+            case 'index':
+                include 'pages/home.php';
+                break;
+            case 'about':
+                include 'pages/about.php';
+                break;
+            case 'color':
+                include 'pages/color.php';
+                break;
+        }
+    ?>
 </body>
 
 </html>
