98a6b7681f74ba5b1802c7406c8e6df68ea8d73d
Luke Livingston <luke.livingston@colostate.edu>
2026-04-09 15:19:42 -0600

    About borders for profiles

--- pages/about.php
+++ pages/about.php
@@ -6,7 +6,7 @@ $page = "about";
             <h1>Meet the Team</h1>
             <p>We are highly dedicated programmers who love to write backend code. We are committed to delivering the best possible color generator, with ease of use for the end user. </p>        </div>
         <div class="div4">
-            <img src="./images/lukeProfile.jpg" alt=profile photo of luke></img>
+            <img class="profilePhoto" src="./images/lukeProfile.jpg" alt=profile photo of luke></img>
             <h4>Luke Livingston</h4>
             <h5>Programmer</h5>
             <p>Luke is a hard working member of this team, contributing to much of the styling for the website and some php scripting.
@@ -14,7 +14,7 @@ $page = "about";
             </p>
         </div>
         <div class="div5">
-            <img src="./images/TylerProfile.JPG" alt=profile photo of the big T himself></img>
+            <img class="profilePhoto" src="./images/TylerProfile.JPG" alt=profile photo of the big T himself></img>
             <h4>Tyler Couse</h4>
             <h5>Programmer</h5>
             <p>Tyler is an extremely devoted computer science student. He has contributed heavily to the front end. 
@@ -22,7 +22,7 @@ $page = "about";
             </p>
         </div>
         <div class="div6">
-            <img src="./images/AaronProfile.JPG" alt=profile photo of aaron></img>
+            <img class="profilePhoto" src="./images/AaronProfile.JPG" alt=profile photo of aaron></img>
             <h4>Aaron Barnes</h4>
             <h5>Programmer</h5>
             <p>Aaron is a spirit of scrum and software development, he loves to coloborate with others and share his knowledge.
@@ -30,7 +30,7 @@ $page = "about";
             </p>
         </div>
         <div class="div7">
-            <img src="./images/ConorProfile.JPG" alt=profile photo of conor></img>
+            <img class="profilePhoto" src="./images/ConorProfile.JPG" alt=profile photo of conor></img>
             <h4>Conor Holland</h4>
             <h5>Programmer</h5>
             <p>Conor is a passionate developer who loves to create innovative solutions. Originally from Scotland, Conor brings a unique perspective to our development team.
