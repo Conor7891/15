c71334cb7cb3750827e07df5ec54d028ff9259ce
Luke Livingston <luke.livingston@colostate.edu>
2026-04-27 14:02:20 -0600

    Print button and color button fix

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
         <p>Generating a color palette can be hard, so here at PixelPushers, we have developed a tool to quickly and easily create color palletes that you can
                 use for website design, paintings, presentations, or more! Simply press the button below to start.
         </p>
-        <form method="POST">
+        <form method="GET">
             <button id="ColorButton" type="submit" name="page" value="color">Try the Color Coordinator</button>
         </form>
     </div>
